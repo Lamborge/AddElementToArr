02df37261d8fc40d41fc8d761c8d499db5a8bf5e
Lamborge <maxpopelov@gmail.com>
2020-12-06 16:27:39 +0300

    added no complete void to add element to array

--- Program.cs
+++ Program.cs
@@ -6,27 +6,30 @@ namespace AddElementToArr
     {
 
         public int[] arr = { 0,1,2,3,4,5,6,7,8,9};
-        
 
-        static void Main(string[] args)
+        public static void AddElToArr(int enter_array[], int element)
         {
-            
-        }
 
-        public int[] AddElToArr(int enter_array[], int element)
-        {
             int i = 0;
             string temp = String.Join(".", enter_array[]);
             char[] temp_char = temp.ToCharArray();
             foreach (var item in temp_char)
             {
-                if(item == '.')
+                if (item == '.')
                 {
                     i++;
                 }
             }
 
+            int[] res_arr = new int[i + 1];
+            res_arr[i + 1] = element;
+
             return res_arr[];
+        }
+
+        static void Main(string[] args)
+        {
+            //Todo
         }
     }
 }
